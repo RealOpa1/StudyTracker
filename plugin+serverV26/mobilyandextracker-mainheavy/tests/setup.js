b93efe3202442430d5b7623ffdac961345403264
RealOpa1 <163524906+RealOpa1@users.noreply.github.com>
2026-06-22 01:57:11 +0300

    Delete plugin+serverV26/mobilyandextracker-mainheavy directory

--- plugin+serverV26/mobilyandextracker-mainheavy/tests/setup.js
+++
@@ -1,8 +0,0 @@
-globalThis.__EDU = undefined;
-globalThis.browser = {
-  storage: { local: { get: jest.fn(), set: jest.fn() } },
-  tabs: { query: jest.fn(), onUpdated: { addListener: jest.fn() }, onRemoved: { addListener: jest.fn() } },
-  runtime: { sendMessage: jest.fn(), onMessage: { addListener: jest.fn() } },
-  alarms: { create: jest.fn(), onAlarm: { addListener: jest.fn() } },
-  action: {}
-};
